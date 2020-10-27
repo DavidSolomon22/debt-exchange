de497f061dd9f2be658a506e108accd3dd859c69
David Solomon <dav.soli22@gmail.com>
2020-10-27 13:13:11 +0100

    Rgister filters globally and add pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MongoExceptionFilter, MongooseExceptionFilter } from './filters';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(
+    new MongooseExceptionFilter(),
+    new MongoExceptionFilter(),
+  );
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
---
+++ src/pipes/parse-sort-params.pipe.ts
@@ -0,0 +1,22 @@
+import { PipeTransform, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ParseSortParamsPipe implements PipeTransform {
+  transform(value: string[]): string {
+    if (value.length === 0) {
+      return null;
+    } else {
+      return value
+        .map(sortParam => {
+          if (sortParam.includes(' desc')) {
+            return `-${sortParam.replace(' desc', '')}`;
+          } else {
+            return sortParam;
+          }
+        })
+        .reduce((previousSortParam, currentSortParam) => {
+          return `${previousSortParam} ${currentSortParam}`;
+        });
+    }
+  }
+}
